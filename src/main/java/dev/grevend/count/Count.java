35baddc777e2751fa15a63650226fc0b440bf360
grevend <55140355+grevend@users.noreply.github.com>
2021-03-14 18:19:44 +0100

    Add class and main method documentation

--- src/main/java/dev/grevend/count/Count.java
+++ src/main/java/dev/grevend/count/Count.java
@@ -6,12 +6,24 @@ import java.util.concurrent.Callable;
 
 import static picocli.CommandLine.Command;
 
-@Command(name = "count", version = "count 1.0", description = "Count the human readable characters, lines or words from stdin or a file and write the number to stdout or a file.")
+/**
+ * The count command entry class.
+ *
+ * @since sprint 1
+ */
+@Command(name = "count", version = "count 1.0",
+    description = "Count the human readable characters, lines or words from stdin or a file and write the number to stdout or a file.")
 public class Count implements Callable<Integer> {
 
+    /**
+     * Main program entry point. Creates a command line instance and delegates the given program args to the count command.
+     *
+     * @param args the supplied program args
+     *
+     * @since sprint 1
+     */
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new Count()).execute("");
-        System.exit(exitCode);
+        System.exit(new CommandLine(new Count()).execute(args));
     }
 
     @Override
