a5f9856a28b506a67776bb321515d5b9e0a282a1
grevend <55140355+grevend@users.noreply.github.com>
2021-03-16 22:38:23 +0100

    Change implemented interface from ToIntFunction to Function<String, IntStream>
    Move humanReadable field from Count into CountingMethods (+ make static)

--- src/main/java/dev/grevend/count/CountingMethods.java
+++ src/main/java/dev/grevend/count/CountingMethods.java
@@ -1,32 +1,40 @@
 package dev.grevend.count;
 
-import java.util.function.ToIntFunction;
+import java.util.function.Function;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
-public enum CountingMethods implements ToIntFunction<String> {
+public enum CountingMethods implements Function<String, IntStream> {
 
     chars() {
 
         @Override
-        public int applyAsInt(String line) {
-            return 0;
+        public IntStream apply(String line) {
+            return humanReadable.matcher(line.strip())
+                .results()
+                .map(MatchResult::group)
+                .mapToInt(String::length);
         }
 
     },
     words() {
 
         @Override
-        public int applyAsInt(String value) {
-            return 0;
+        public IntStream apply(String line) {
+            return IntStream.empty();
         }
 
     },
     lines() {
-        
+
         @Override
-        public int applyAsInt(String value) {
-            return 0;
+        public IntStream apply(String line) {
+            return IntStream.empty();
         }
 
-    }
+    };
+
+    private static final Pattern humanReadable = Pattern.compile("[^\\p{C}\\p{Z}]+", Pattern.UNICODE_CHARACTER_CLASS);
 
 }
