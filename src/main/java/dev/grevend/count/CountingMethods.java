0c13d8e6309650f5543ad7af4d86f688b02045c5
grevend <55140355+grevend@users.noreply.github.com>
2021-03-16 22:31:48 +0100

    Implement ToIntFunction<String> for enum CountingMethods

--- src/main/java/dev/grevend/count/CountingMethods.java
+++ src/main/java/dev/grevend/count/CountingMethods.java
@@ -1,5 +1,32 @@
 package dev.grevend.count;
 
-public enum CountingMethods {
-    chars, words, lines
+import java.util.function.ToIntFunction;
+
+public enum CountingMethods implements ToIntFunction<String> {
+
+    chars() {
+
+        @Override
+        public int applyAsInt(String line) {
+            return 0;
+        }
+
+    },
+    words() {
+
+        @Override
+        public int applyAsInt(String value) {
+            return 0;
+        }
+
+    },
+    lines() {
+        
+        @Override
+        public int applyAsInt(String value) {
+            return 0;
+        }
+
+    }
+
 }
