6518935a6551bd1315fe3771717118d87c316426
grevend <55140355+grevend@users.noreply.github.com>
2021-03-17 11:24:41 +0100

    Add CountingMethods.words.apply(String) method documentation
    Update CountingMethods documentation to reflect move to long streams

--- src/main/java/dev/grevend/count/CountingMethods.java
+++ src/main/java/dev/grevend/count/CountingMethods.java
@@ -13,7 +13,7 @@ public enum CountingMethods implements Function<String, LongStream> {
          * Computes the character counts / lengths of all matched groups based on the humanReadable pattern.
          *
          * @param line the current text chunk
-         * @return an int stream of counts
+         * @return a long stream of counts
          *
          * @since sprint 1
          */
@@ -24,6 +24,14 @@ public enum CountingMethods implements Function<String, LongStream> {
 
     }, words() {
 
+        /**
+         * Computes the word count of the given text.
+         *
+         * @param line the current text chunk
+         * @return a long stream containing the word count
+         *
+         * @since sprint 1
+         */
         @Override
         public LongStream apply(String line) {
             return LongStream.of(word.matcher(line).results().count());
@@ -33,8 +41,9 @@ public enum CountingMethods implements Function<String, LongStream> {
 
         /**
          * Computes the line count by checking if the text is blank.
+         *
          * @param line the current text chunk
-         * @return an int stream that contains zero if the chunk is blank or else 1
+         * @return a long stream that contains zero if the chunk is blank or else 1
          *
          * @since sprint 1
          */
