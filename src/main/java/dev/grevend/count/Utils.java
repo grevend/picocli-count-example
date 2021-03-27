757f5b4423e78c512227a0b882495b9d91cafa3b
grevend <55140355+grevend@users.noreply.github.com>
2021-03-27 22:16:44 +0100

    Add exists(File, PrintWriter) and writable(File, PrintWriter) util methods

--- src/main/java/dev/grevend/count/Utils.java
+++ src/main/java/dev/grevend/count/Utils.java
@@ -1,8 +1,7 @@
 package dev.grevend.count;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UncheckedIOException;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Spliterator;
@@ -50,6 +49,56 @@ public final class Utils {
                 }
             }
         }, Spliterator.ORDERED | Spliterator.NONNULL), false) : Stream.empty();
+    }
+
+    /**
+     * Returns whether the given {@code File} exists.
+     *
+     * @param file the {@code File} to check
+     * @param err  the {@code PrintWriter} provided for error output
+     *
+     * @return if the {@code File} exists
+     *
+     * @since sprint 2
+     */
+    public static boolean exists(File file, PrintWriter err) {
+        var does = file != null && file.exists() && !file.isDirectory();
+        if (!does && file != null) {
+            if (file.isDirectory()) {
+                err.println("Output path points to a directory!");
+                return false;
+            } else {
+                try {
+                    if (!file.exists() && !file.createNewFile()) {
+                        throw new IOException();
+                    } else {
+                        return true;
+                    }
+                } catch (IOException | SecurityException e) {
+                    err.println("Failed to create output file!");
+                    return false;
+                }
+            }
+        }
+        return does;
+    }
+
+    /**
+     * Returns whether the given {@code File} is writable.
+     *
+     * @param file the {@code File} to check
+     * @param err  the {@code PrintWriter} provided for error output
+     *
+     * @return if the {@code File} is writable
+     *
+     * @since sprint 2
+     */
+    public static boolean writable(File file, PrintWriter err) {
+        var is = exists(file, err) && file != null && Files.isWritable(file.toPath());
+        if (!is && file != null && file.exists()) {
+            err.println("Output file is read-only!");
+        }
+        return is;
     }
 
 }
